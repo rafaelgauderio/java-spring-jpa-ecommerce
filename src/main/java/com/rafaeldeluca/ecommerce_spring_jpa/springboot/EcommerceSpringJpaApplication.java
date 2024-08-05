0e13438c3719b9a7d4ec0be20f91f8936ab57bfb
Rafael de Luca <deluca1712@gmail.com>
2024-08-05 16:09:49 -0300

    Create project and download depencencies

---
+++ src/main/java/com/rafaeldeluca/ecommerce_spring_jpa/springboot/EcommerceSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.rafaeldeluca.ecommerce_spring_jpa.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceSpringJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaeldeluca/ecommerce_spring_jpa/springboot/EcommerceSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaeldeluca.ecommerce_spring_jpa.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceSpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
