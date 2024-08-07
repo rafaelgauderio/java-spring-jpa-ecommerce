8d88d8d760426b105acde704d544d00c9dbfa16b
Rafael de Luca <deluca1712@gmail.com>
2024-08-07 16:59:22 -0300

    entity Product and bacic jpa annotations

---
+++ src/main/java/com/rafaeldeluca/ecommerce_spring_jpa/springboot/entity/Product.java
@@ -0,0 +1,75 @@
+package com.rafaeldeluca.ecommerce_spring_jpa.springboot.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+public class Product {
+
+    @Id
+    private Long Id;
+    private String name;
+    private String description;
+    private String sku;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastUpdatedDate;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public LocalDateTime getLastUpdatedDate() {
+        return lastUpdatedDate;
+    }
+
+    public void setLastUpdatedDate(LocalDateTime lastUpdatedDate) {
+        this.lastUpdatedDate = lastUpdatedDate;
+    }
+}
